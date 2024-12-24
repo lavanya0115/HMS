a88c4c8c56d6d50adde1b2c2b8922dafce8252a5
Lavanya <lavanyam@agilehealth.in>
2024-12-24 16:06:50 +0530

    feat: table created

---
+++ database/migrations/2024_12_24_155248_create_menu_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('category_id');
+            $table->text('description')->nullable();
+            $table->integer('nos');
+            $table->decimal('price');
+            $table->json('meta')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menu_items');
+    }
+};
