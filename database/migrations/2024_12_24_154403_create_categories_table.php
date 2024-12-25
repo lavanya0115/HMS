f67ae3c89eccf71bfcb10758781165d255c8e427
Lavanya <lavanyam@agilehealth.in>
2024-12-25 16:17:14 +0530

    feat: activity log

--- database/migrations/2024_12_24_154403_create_categories_table.php
+++ database/migrations/2024_12_24_154403_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->boolean('is_active');
             $table->text('description')->nullable();
             $table->json('meta')->nullable();
             $table->unsignedBigInteger('created_by');
