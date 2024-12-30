40ff268ae3102fd83cfe82b3122397fcd6f6608c
Lavanya <lavanyam@agilehealth.in>
2024-12-30 16:50:07 +0530

    feat: menu items listed

--- app/Http/Livewire/MenuCard.php
+++ app/Http/Livewire/MenuCard.php
@@ -4,13 +4,30 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\MenuItem;
+use Illuminate\Support\Carbon;
 
 class MenuCard extends Component
 {
     public function render()
     {
+        $currentHour = Carbon::now()->hour;
+        $currentCategory = '';
+        if ($currentHour >= 8 && $currentHour < 12) {
+            $currentCategory = 'Break fast';
+        } elseif ($currentHour >= 12 && $currentHour < 17) {
+            $currentCategory = 'Lunch';
+        } else {
+            $currentCategory = 'Dinner';
+        }
+
         $menuItems = MenuItem::with('category')
-            ->where('is_available', true)
+            ->whereHas('category', function ($query) use ($currentCategory) {
+                $query->where('title', $currentCategory);
+            })
+            ->orWhereHas('category', function ($query) {
+                $query->where('title', 'Refreshment');
+            })
+            ->orderByDesc('is_available')
             ->get()
             ->groupBy('category.title');
 
