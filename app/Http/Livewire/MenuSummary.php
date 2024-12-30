46a9c7bee01879175239185a00d149f81bd3716e
Lavanya <lavanyam@agilehealth.in>
2024-12-30 13:15:40 +0530

    feat: multiselect added for updation and deletion

--- app/Http/Livewire/MenuSummary.php
+++ app/Http/Livewire/MenuSummary.php
@@ -14,6 +14,7 @@ class MenuSummary extends Component
     protected $listeners = [
         'callShowNoticeEvent' => 'showNoticeListener',
         'deleteMenu' => 'deleteMenuById',
+        'deleteSelected' => 'deleteSelectedMenuIds',
     ];
 
     protected $paginationTheme = 'bootstrap';
@@ -21,6 +22,78 @@ class MenuSummary extends Component
     public $perPage = 10;
 
     public $menuId = null;
+    // public $menuItemsAll;
+
+    public $selectedItems = [];
+    public $selectedAll = false;
+
+    public $menu = [
+        'is_available',
+        'custom_status',
+    ];
+
+    public function toggleSelectAll()
+    {
+        if ($this->selectedAll) {
+            $this->selectedItems = $this->getMenuItems()->pluck('id')->toArray();
+        } else {
+            $this->selectedItems = [];
+        }
+    }
+    // public function updatedSelectedItems()
+    // {
+    //     dd('kl',  $this->selectedAll, $this->selectedItems);
+    //     $this->selectedAll = count($this->selectedItems) === $this->getMenuItems()->count();
+    //     dd($this->selectedItems);
+    // }
+    public function getSelectedItems()
+    {
+        $this->selectedItems;
+        // dd($this->selectedItems);
+        $menus = MenuItem::whereIn('id', $this->selectedItems)->get();
+        $this->menu['is_available'] = $menus->pluck('is_available')->first() ? true : false;
+        $this->menu['custom_status'] = $menus->pluck('custom_status')->first();
+    }
+
+    public function deleteSelectedMenuIds()
+    {
+        // dd( $this->selectedItems);
+        $menuItems = MenuItem::whereIn('id', $this->selectedItems)->get();
+        if ($menuItems) {
+            foreach ($menuItems as $menu) {
+                $menu->update([
+                    'deleted_by' => getAuthData()->id,
+                ]);
+                $menu->delete();
+            }
+        }
+        $this->selectedItems = [];
+        $this->selectedAll = false;
+        session()->flash('message', 'Selected items deleted successfully.');
+    }
+
+    public function updateStatus()
+    {
+        $menuItems = MenuItem::whereIn('id', $this->selectedItems)->get();
+        if ($menuItems) {
+            foreach ($menuItems as $item) {
+                $item->update([
+                    'is_available' => $this->menu['is_available'],
+                    'custom_status' => $this->menu['custom_status'],
+                ]);
+                $isUpdated = $item->wasChanged('is_available', 'custom_status');
+            }
+
+            if ($isUpdated) {
+                session()->flash('success', 'Selected items updated successfully.');
+                return redirect(route('menu.items.list'));
+            } else {
+                session()->flash("error", "Unable to update the menus");
+                return redirect(route('menu.items.list'));
+            }
+        }
+    }
+
 
     public function mount(Request $request)
     {
@@ -30,13 +103,17 @@ class MenuSummary extends Component
     {
         session()->flash($status, $message);
     }
+
+    public function getMenuItems()
+    {
+        $menuItems = MenuItem::orderBy('name')
+            ->paginate($this->perPage);
+        return $menuItems;
+    }
     public function render()
     {
         $user = getAuthData();
-
-        $menuItems = MenuItem::orderBy('name')
-            ->paginate($this->perPage, pageName: 'p');
-
+        $menuItems = $this->getMenuItems();
         return view('livewire.menu-summary', [
             'menuItems' => $menuItems,
         ])->layout('layouts.admin');
