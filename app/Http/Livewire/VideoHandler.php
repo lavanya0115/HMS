2bac78160ee4a962eb5860ac5ef197f05b98a294
Lavanya <lavanyam@agilehealth.in>
2025-01-24 11:22:08 +0530

    feat: create fn added

--- app/Http/Livewire/VideoHandler.php
+++ app/Http/Livewire/VideoHandler.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Models\Video;
 use Livewire\Component;
+use Illuminate\Http\Request;
 use Livewire\WithFileUploads;
 
 class VideoHandler extends Component
@@ -14,7 +15,7 @@ class VideoHandler extends Component
     public $video;
 
     protected $rules = [
-        'video' => 'required|mimes:mp4,avi,mkv|max:50000',
+        'video' => 'required',
     ];
 
     protected $messages = [
@@ -25,21 +26,42 @@ class VideoHandler extends Component
     {
         $this->videoId = $videoId;
     }
-    public function create()
+    public function updatedVideo()
+    {
+        dd($this->video);
+    }
+    public function create(Request $request)
     {
         $this->validate();
         $authId = getAuthData()->id;
 
         try {
-            dd($this->video);
-            // $video = Video::create([
-            //     'title' =>,
-            //     'path' => ,
-            //     'format' => ,
-            //     'size' =>,
-            //     'created_by' =>,
-            //     'updated_by' =>,
-            // ]);
+            dd($this->video,$request->file('video'));
+
+            $file = $request->file('vidoe');
+
+            $fileName = $file->getClientOriginalName();
+            $fileExtension = $file->getClientOriginalExtension();
+            $fileSize = $file->getSize();
+            $fileMimeType = $file->getMimeType();
+
+            $fileSizeInMB = round($fileSize / (1024 * 1024), 2);
+
+            $random = now()->format('H:i');
+            $uniqueFileName = $fileName . '_' . $random;
+            $fileDirectory = 'videos/';
+            if (!file_exists($fileDirectory)) {
+                mkdir($fileDirectory, 0755, true);
+            }
+            $filePath = $file->storeAs($fileDirectory, $uniqueFileName, 'public');
+            $video = Video::create([
+                'title' => $fileName,
+                'path' => $filePath,
+                'format' => $fileMimeType,
+                'size' => $fileSizeInMB . ' MB',
+                'created_by' => $authId,
+                'updated_by' => $authId,
+            ]);
             if ($video) {
                 session()->flash('success', 'Video Uploaded successfully!.');
                 return redirect(route('category'));
--- resources/views/livewire/video-handler.blade.php
+++ resources/views/livewire/video-handler.blade.php
@@ -1,24 +1,27 @@
 <div>
     <h4>New</h4>
     <div class="card">
-        <form wire:submit.prevent="create">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="row row-cards">
-                            <div class="col-md-12">
-                                <div class="row">
-                                    <div class="col-md-12 mb-3">
-                                        <label class="form-label required">
-                                            Upload Video
-                                        </label>
-                                        <div>
-                                            <input type="file" accept="video/*" id="video" wire:model="video"
-                                                class="form-control">
-                                            @error('video')
-                                                <span class="text text-danger">{{ $message }}</span>
-                                            @enderror
+        {{-- <form wire:submit.prevent=>
+            @csrf  --}}
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row row-cards">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="mb-2">
+                                        <div class="avatar-edit ">
+                                            <label class="form-label requred" for="video">Video Upload</label>
+                                            <input type='file' class="form-control" wire:model="video" id="video"
+                                                name="video" />
+                                            <span wire:loading wire:target="video" class="text-info fw-bold mt-2 fs-5">
+                                                Uploading...
+                                            </span>
                                         </div>
+                                        @error('video')
+                                            <span class="text text-danger">{{ $message }}</span>
+                                        @enderror
                                     </div>
                                 </div>
                             </div>
@@ -26,10 +29,11 @@
                     </div>
                 </div>
             </div>
-            <div class="card-footer text-end">
-                <a href={{ route('video') }} class="text-danger me-2 text-decoration-none"> Cancel </a>
-                <button type="submit" class="btn btn-primary ">Upload</button>
-            </div>
-        </form>
+        </div>
+        <div class="card-footer text-end">
+            <a href={{ route('video') }} class="text-danger me-2 text-decoration-none"> Cancel </a>
+            <button wire:click.prevent="create" class="btn btn-primary ">Upload</button>
+        </div>
+        {{-- </form> --}}
     </div>
 </div>
